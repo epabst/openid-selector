6ea1eeab24239e5d59dde012964fea4286b66fbf
Richard Gomes <rgomes.info@gmail.com>
2013-10-29 18:18:04 +0000

    Fix troubles when installing from PyPI

--- setup.py
+++ setup.py
@@ -21,29 +21,34 @@ long_description = (
 
 def staging(pkg, entries, init=True, rmdir=True):
     import os, shutil
-    find_packages = []
+    packages = []
+    package_data = {}
     if os.path.isdir(pkg):
         print('Staging folder {} already exists. Skipping...'.format(pkg))
-        find_packages.append(pkg)
+        packages.append(pkg)
         for entry in entries:
             (from_dir, to_dir, files) = entry
-            find_packages.append('.'.join([pkg, to_dir]))
+            subpkg = '.'.join([pkg, to_dir])
+            packages.append(subpkg)
+            package_data[subpkg] = files
     else:
         os.mkdir(pkg)
         if init:
             open('/'.join([pkg, '__init__.py']), 'a').close()
-            find_packages.append(pkg)
+            packages.append(pkg)
         for entry in entries:
             (from_dir, to_dir, files) = entry
             os.mkdir('/'.join([pkg, to_dir]))
             if init:
                 open('/'.join([pkg, to_dir, '__init__.py']), 'a').close()
-                find_packages.append('.'.join([pkg, to_dir]))
             for file in files:
                 src = '/'.join([from_dir, file])
                 dst = '/'.join([pkg, to_dir, file])
                 shutil.copyfile(src, dst)
-    return find_packages
+            subpkg = '.'.join([pkg, to_dir])
+            packages.append(subpkg)
+            package_data[subpkg] = files
+    return (packages, package_data)
 
 
 pkg = 'openid_selector'
@@ -51,6 +56,9 @@ staging_entries = \
          [ ('js',     'js',     [ 'jquery-1.2.6.min.js', 'openid-en.js', 'openid-jquery.js' ]), \
            ('css',    'css',    [ 'openid.css', 'openid-shadow.css' ]), \
            ('images', 'images', [ 'openid-inputicon.gif', 'openid-providers-en.png' ]) ]
+
+(packages, package_data) = staging(pkg, staging_entries)
+
 
 setup(name='openid-selector',
       version=version,
@@ -65,8 +73,9 @@ setup(name='openid-selector',
       author_email='rgomes.info@gmail.com',
       url='http://openid-selector.readthedocs.org',
       license='BSD',
-      packages=staging(pkg, staging_entries),
-      include_package_data=True,
+      packages=packages,
+      package_data=package_data,
+      include_package_data=False,
       zip_safe=False,
       install_requires=[ 'setuptools' ],
       )
